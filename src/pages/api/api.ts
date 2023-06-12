a24fb280791c88f47d7e984092759044d7141a28
PAYNAMICS <paynamics@192.168.1.14>
2023-06-13 02:41:16 +0800

    newendpoint

--- src/pages/api/api.ts
+++ src/pages/api/api.ts
@@ -1,13 +1,21 @@
 import axios from 'axios';
 
 export const getPropertyListing = () => {
-  return axios.get('https://9c92-136-158-25-84.ngrok-free.app/v1/test/property-fetching/APPROVED');
-};
+  return axios.get('https://c868-136-158-25-84.ngrok-free.app/v1/test/property-fetching/APPROVED', { headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Methods': '*',
+      'ngrok-skip-browser-warning':  true
+    }
+  });
+  };
 
 export const getUser = () => {
-  return axios.get ('https://9c92-136-158-25-84.ngrok-free.app/v1/test/User-fetching');
+  return axios.get ('https://c868-136-158-25-84.ngrok-free.app/v1/test/User-fetching', { headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': '*',
+    'ngrok-skip-browser-warning':  true
+  }
+});
 };
-
-
-
-
